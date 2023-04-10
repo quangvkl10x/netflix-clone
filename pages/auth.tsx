b83b8fe1677475356ceb9d05dc7355f634b32c47
nvq2309 <nvq2309@gmail.com>
2023-04-11 00:37:35 +0700

    Update register, login, auth, prisma

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,46 @@
 import Input from "@/components/input";
-import { use, useState } from "react";
+import { use, useCallback, useState } from "react";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/profiles",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [email, password]);
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+      await login();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [email, name, password, login]);
 
   return (
     <>
@@ -16,17 +52,19 @@ const Auth = () => {
           <div className="flex justify-center">
             <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
               <h2 className="text-white text-4xl mb-8 font-semibold">
-                Sign in
+                {variant === "login" ? "Sign in" : "Register"}
               </h2>
               <div className="flex flex-col gap-4">
-                <Input
-                  id="name"
-                  label="Username"
-                  value={name}
-                  onChange={(e: any) => {
-                    setName(e.target.value);
-                  }}
-                />
+                {variant === "register" && (
+                  <Input
+                    id="name"
+                    label="Username"
+                    value={name}
+                    onChange={(e: any) => {
+                      setName(e.target.value);
+                    }}
+                  />
+                )}
                 <Input
                   id="email"
                   label="Email"
@@ -44,13 +82,39 @@ const Auth = () => {
                   }}
                   type="password"
                 />
-                <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700">
-                  Login
+                <button
+                  onClick={variant === "register" ? register : login}
+                  className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+                >
+                  {variant === "login" ? "Login" : "Register"}
                 </button>
+                <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                  <div
+                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                    onClick={() =>
+                      signIn("google", { callbackUrl: "/profiles" })
+                    }
+                  >
+                    <FcGoogle size={30} />
+                  </div>
+                  <div
+                    className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                    onClick={() =>
+                      signIn("github", { callbackUrl: "/profiles" })
+                    }
+                  >
+                    <FaGithub size={30} />
+                  </div>
+                </div>
                 <p className="text-neutral-500 mt-12">
-                  First time using Netflix?
-                  <span className="text-white ml-1 hover:underline cursor-pointer">
-                    Create new account! 
+                  {variant === "login"
+                    ? "First time using Netflix?"
+                    : "Already have an account?"}
+                  <span
+                    className="text-white ml-1 hover:underline cursor-pointer"
+                    onClick={toggleVariant}
+                  >
+                    {variant === "login" ? "Create new account!" : "Login"}
                   </span>
                 </p>
               </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import useCurrnetUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
 
-const inter = Inter({ subsets: ["latin"] });
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrnetUser();
+
   return (
     <>
       <h1 className="text-2xl text-green-500">Netflix</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 }
